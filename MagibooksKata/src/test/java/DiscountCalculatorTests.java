24b17af11336490d44f5d279d212e79b1fc5f40d
Ben Sloan <ben.sloan@digital.homeoffice.gov.uk>
2024-06-24 15:03:52 +0100

    Next failing test.

--- MagibooksKata/src/test/java/DiscountCalculatorTests.java
+++ MagibooksKata/src/test/java/DiscountCalculatorTests.java
@@ -51,6 +51,48 @@ public class DiscountCalculatorTests {
         assertEquals(2.4f, discountAmountPounds);
     }
 
+    @Test
+    void whenFourDifferentBooks_ThenDiscountApplied() {
+        Basket basket = new Basket();
+
+        Book bookOne = new Book(1);
+        Book bookTwo = new Book(2);
+        Book bookThree = new Book(3);
+        Book bookFour = new Book(4);
+
+        basket.addBook(bookOne);
+        basket.addBook(bookTwo);
+        basket.addBook(bookThree);
+        basket.addBook(bookFour);
+
+        SeriesMultiBuyDiscountCalculator calculator = new SeriesMultiBuyDiscountCalculator(basket);
+        float discountAmountPounds = calculator.getDiscountAmountPounds();
+
+        assertEquals(6.4f, discountAmountPounds);
+    }
+
+    @Test
+    void whenFiveDifferentBooks_ThenDiscountApplied() {
+        Basket basket = new Basket();
+
+        Book bookOne = new Book(1);
+        Book bookTwo = new Book(2);
+        Book bookThree = new Book(3);
+        Book bookFour = new Book(4);
+        Book bookFive = new Book(5);
+
+        basket.addBook(bookOne);
+        basket.addBook(bookTwo);
+        basket.addBook(bookThree);
+        basket.addBook(bookFour);
+        basket.addBook(bookFive);
+
+        SeriesMultiBuyDiscountCalculator calculator = new SeriesMultiBuyDiscountCalculator(basket);
+        float discountAmountPounds = calculator.getDiscountAmountPounds();
+
+        assertEquals(10f, discountAmountPounds);
+    }
+
 }
 
 
